8fa9ec6158e4b23b820e843c6228c9d4f2606c36
Krithi <krithis2991@gmail.com>
2019-08-20 21:22:19 -0400

    post and get methods works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,29 @@
 //GET route - url - /api/friends --JSON of all posiible fiends
 //POST route - url - /api/friends -- store all submitted interests.
+const friends= require("../data/friends.js")
+
+module.exports = function(app){
+
+    app.get("/friends", function(req, res){
+        console.log(friends);
+        return res.json(friends);
+    });
+    
+    app.post("/survey", function(req,res){
+    
+        var newPerson = req.body;
+    
+        console.log(newPerson);
+    
+        friends.push(newPerson);
+    
+        res.json(newPerson);
+
+        console.log(friends);
+        
+    });
+}
+
+
+
+
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,12 @@
 //GET route - /survey -- display survey.html
 //Catch all- routes - default route - dispalys home.html
+
+module.exports = function(app, path) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+}
