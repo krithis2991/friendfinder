634b18b95abd7f04b3d0f984ab9a34937b585db8
Krithi <krithis2991@gmail.com>
2019-08-21 10:25:05 -0400

    Changed the friend.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,40 +1,47 @@
-//Holds the array of objects of friends.
-//Sample - Data
-// {
-//     "name":"Ahmed",
-//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//     "scores":[
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-//   }
-
 let friendsArr = [
     {
         "name": "Bob",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-           4,
-           4,
-           5,
-           1,
-           2,
-           5,
-           4,
-           1
-        ]
+        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
+    },
+    {
+        "name": "Kate",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
+    },
+    {
+        "name": "Jim",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
+    },
+    {
+        "name": "Pam",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
+    },
+    {
+        "name": "Kevin",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
+    },
+    {
+        "name": "Erin",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
+    },
+    {
+        "name": "Andrea",
+        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
+    },
+    {
+        "name": "Mindy",
+        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
     }
 ];
+
+
+
+
 
 module.exports= friendsArr;
 
