84b2ac85d927ea8713dc9e58c3cecbede6ce5629
Krithi <krithis2991@gmail.com>
2019-08-22 11:22:57 -0400

    Mathes found

--- app/data/friends.js
+++ app/data/friends.js
@@ -12,7 +12,7 @@ let friendsArr = [
     {
         "name": "Jim",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
+        "scores": [ 1,1,2,4,1,1,2,1,1,1 ]
     },
     {
         "name": "Pam",
@@ -31,17 +31,15 @@ let friendsArr = [
     },
     {
         "name": "Andrea",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
     },
     {
         "name": "Mindy",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [ 5,1,4,4,5,1,2,5,4,1 ]
     }
 ];
-
-
-
-
 
 module.exports= friendsArr;
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,24 +4,67 @@ const friends= require("../data/friends.js")
 
 module.exports = function(app){
 
-    app.get("/friends", function(req, res){
+    app.get("/api/friends", function(req, res){
         console.log(friends);
         return res.json(friends);
     });
+
+    app.get("/friends", function(req,res){
+        res.json(friends);
+    })
     
     app.post("/survey", function(req,res){
-    
-        var newPerson = req.body;
-    
-        console.log(newPerson);
-    
-        friends.push(newPerson);
-    
-        res.json(newPerson);
 
-        console.log(friends);
+        const friendMatch = {
+            name: "",
+            photo: "",
+            // scoreDifference : 1000
+        };
+    
+        const newPerson = req.body;
+        //console.log(newPerson);
+
+        let newName = newPerson.name;
+        let newScore = newPerson.scores;
+
+        let totalScore = newScore.forEach(function (e){
+           parseInt.a ;
+           console.log(e);
+        });
+
+        // newPerson = {
+        //     name: req.body.name,
+        //     photo: req.body.photo,
+        //     scores: totalScore
+        // };
+
+        let lowestDifference = 1000;
+        let bestMatch = {};
+
+        for(let i = 0; i<friends.length; i++){
+            console.log(friends[i].name);
+            let scoreDifference = 0;
+            for (let j = 0; j < friends[i].scores.length; j++) {
+                console.log('firstNum', parseInt(friends[i].scores[j]), 'secondNum', parseInt(newScore[j]));
+                scoreDifference += Math.abs(parseInt(friends[i].scores[j] - parseInt(newScore[j])));
+            }
+
+            if (scoreDifference < lowestDifference) {
+                lowestDifference = scoreDifference;
+                bestMatch = friends[i];
+            }
+
+            console.log("Total Difference " +scoreDifference);
+            console.log("Best match friend diff" + JSON.stringify(bestMatch));
+            
+            
+        }
+
+        friends.push(newPerson);
+        res.json(bestMatch);
+        // console.log(friends);
         
-    });
+    }); 
 }
 
 
